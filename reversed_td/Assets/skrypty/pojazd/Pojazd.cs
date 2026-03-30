a12dd09b577ceba56b3c65387c02b52b17da741b
kacprow21324 <kawoszczylo.work@gmail.com>
2026-03-30 20:54:03 +0200

    Implementacja mechaniki wież do main

---
+++ reversed_td/Assets/skrypty/pojazd/Pojazd.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class pojazd : MonoBehaviour
+{
+    [Header("Ustawienia Zdrowia")]
+    public float maxHp = 100f;
+    private float aktualneHp;
+
+    void Start()
+    {
+        aktualneHp = maxHp;
+    }
+
+    public void OdejmijHp(float obrazenia)
+    {
+        aktualneHp -= obrazenia;
+        if (aktualneHp <= 0f)
+        {
+            Smierc();
+        }
+    }
+
+    void Smierc()
+    {
+        Destroy(gameObject);
+    }
+
+    public float PobierzAktualneHP()
+    {
+        return aktualneHp;
+    }
+}
---
+++ reversed_td/Assets/skrypty/tower/Wieza.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class WiezaAtak : MonoBehaviour
+{
+    [Header("Ustawienia Wie¿y")]
+    public float zasieg = 10f;
+    public float szybkoscAtaku = 1f;  
+    public LayerMask warstwaWroga;
+
+    [Header("Ustawienia Pocisku")]
+    public GameObject prefabPocisku;
+    public Transform punktStrzalu;
+
+    private float licznikAtaku = 0f;
+
+    void Update()
+    {
+        licznikAtaku -= Time.deltaTime;
+
+        if (licznikAtaku <= 0f)
+        {
+            SzukajIAtakuj();
+        }
+    }
+
+    void SzukajIAtakuj()
+    {
+        Collider[] wrogowieWZasiegu = Physics.OverlapSphere(transform.position, zasieg, warstwaWroga);
+
+        if (wrogowieWZasiegu.Length > 0)
+        {
+            Transform cel = wrogowieWZasiegu[0].transform;
+            if (prefabPocisku != null && punktStrzalu != null)
+            {
+                Strzelaj(cel);
+            }
+
+            licznikAtaku = 1f / szybkoscAtaku;
+        }
+    }
+
+    void Strzelaj(Transform cel)
+    {
+        Debug.Log("Wie¿a strzela do: " + cel.name);
+
+        GameObject nowyPociskGO = Instantiate(prefabPocisku, punktStrzalu.position, punktStrzalu.rotation);
+
+        Pocisk skryptPocisku = nowyPociskGO.GetComponent<Pocisk>();
+
+        if (skryptPocisku != null)
+        {
+            skryptPocisku.UstawCel(cel);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, zasieg);
+    }
+}
